9791038fc29834bafaeb17f72a989f39c3f89d02
Gorka <gorka@aquigorka.com>
2018-10-12 17:06:51 +0200

    App: add connect flow

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import { P2PProvider } from './components/P2P'
+import Host from './components/Host'
+import Guest from './components/Guest'
+import Chime from './components/Chime'
 
 class App extends Component {
   render() {
-    return <P2PProvider>React QR Share</P2PProvider>
+    return (
+      <P2PProvider>
+        <Host />
+        <Guest />
+        <Chime />
+      </P2PProvider>
+    )
   }
 }
 
