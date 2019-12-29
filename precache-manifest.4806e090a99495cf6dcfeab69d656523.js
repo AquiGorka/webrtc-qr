292f4094e1a0c683451d9f928796604796abfd5a
Gorka <gorka@aquigorka.com>
2019-12-29 20:08:07 +0100

    Updates

---
+++ precache-manifest.4806e090a99495cf6dcfeab69d656523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0078a81cf94449ee9ec6",
+    "url": "/webrtc-qr/static/css/main.fe50af16.chunk.css"
+  },
+  {
+    "revision": "0078a81cf94449ee9ec6",
+    "url": "/webrtc-qr/static/js/main.0078a81c.chunk.js"
+  },
+  {
+    "revision": "bff9a4245882af52bfdb",
+    "url": "/webrtc-qr/static/js/1.bff9a424.chunk.js"
+  },
+  {
+    "revision": "8fb160ce113200b8691a",
+    "url": "/webrtc-qr/static/js/runtime~main.8fb160ce.js"
+  },
+  {
+    "revision": "972c97d942d542a6625cded283fd81db",
+    "url": "/webrtc-qr/static/media/chime.972c97d9.mp3"
+  },
+  {
+    "revision": "c9840184c02638f435cea96612dc262b",
+    "url": "/webrtc-qr/index.html"
+  }
+];
